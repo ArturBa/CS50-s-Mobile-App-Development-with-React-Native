8e2baa67f595db4ff2c5ece44ce651311b55516b
Artur Bauer <bauerart1@gmail.com>
2020-07-26 19:08:02 +0200

    Add search placeholder

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
+import {Text} from 'react-native';
 
 import MovieListScreen from './screens/MovieListScreen';
 import MovieDetailsScreen from './screens/MovieDetailsScreen';
@@ -10,7 +11,13 @@ const Stack = createStackNavigator();
 function AppStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Movie List" component={MovieListScreen} />
+      <Stack.Screen
+        name="Movie List"
+        component={MovieListScreen}
+        options={() => ({
+          headerRight: () => <Text>Search</Text>,
+        })}
+      />
       <Stack.Screen name="Movie Details" component={MovieDetailsScreen} />
     </Stack.Navigator>
   );
